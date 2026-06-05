fa72994b98d64c526801fe50eb1a96d2d3507b77
Ankit Poudel <pdlankit2000@gmail.com>
2026-06-05 19:27:16 +0545

    completed the last bug. Now I have to finish the main portal

--- Expense Tracker/tracker.py
+++ Expense Tracker/tracker.py
@@ -4,8 +4,8 @@ class ExpenseTracker:
     def __init__(self):
         self.expenses = []
 
-    def add_expense(self,title,amount,category,date):
-        user_expense = Expense(title,amount,category,date)
+    def add_expense(self,name,amount,category,date):
+        user_expense = Expense(name,amount,category,date)
         self.expenses.append(user_expense)
     def get_all_expenses(self):
         for expense in self.expenses:
@@ -16,18 +16,25 @@ class ExpenseTracker:
             total +=temp.amount
         print(f"Total:{total}")  
     def get_category_summary(self):
-        temp_categories =set()
+        temp_categories =dict()
+        # print(temp_categories)    
         for x in self.expenses:
-            temp_categories.add(x.category)
-        print(temp_categories)    
-            
+            if x.category in temp_categories:
+                temp_categories[x.category] += x.amount
+            else:
+                temp_categories[x.category] = x.amount
+        for key,value in temp_categories.items():
+            print(f"{key}:{value}")
 
-
-
-obj1  = ExpenseTracker()
-obj1.add_expense("burgers",5,"food","12 sept")
-obj1.add_expense("veggies",1295,"Health","12 sept")
-obj1.get_category_summary()
+if __name__ =="__main__":
+    obj1 = ExpenseTracker()
+    obj1.add_expense("veggies",1295,"Health","12 sept")
+    obj1.add_expense("burgers",5,"Food","12 sept")
+    obj1.add_expense("Beer",12,"Alcohol","13 may")
+    obj1.add_expense("Vodka",134,"Alcohol","14th may")
+    obj1.get_all_expenses()
+    obj1.get_all_total()
+    obj1.get_category_summary()
 
 # obj1.get_all_expenses()
 # obj1.get_all_total()
